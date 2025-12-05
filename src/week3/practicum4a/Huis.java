a9a8b8875a7265150a8c69b369a9fbcb30b86393
raphaelrebel <raphaelrebel@live.com>
2025-12-05 14:19:55 +0100

    Practicum 4a

---
+++ src/week3/practicum4a/Huis.java
@@ -0,0 +1,24 @@
+package week3.practicum4a;
+
+public class Huis {
+    private String adres;
+    private int bouwjaar;
+    private Persoon huisbaas;
+
+    public Huis(String adr, int bwjr){
+        adres = adr;
+        bouwjaar = bwjr;
+    }
+
+    public void setHuisbaas(Persoon hb){
+        huisbaas = hb;
+    }
+
+    public Persoon getHuisbaas(){
+        return huisbaas;
+    }
+
+    public String toString(){
+        return "Huis " + adres + " is gebouwd in " + bouwjaar + " en heeft huisbaas " + huisbaas;
+    }
+}
---
+++ src/week3/practicum4a/Persoon.java
@@ -0,0 +1,16 @@
+package week3.practicum4a;
+
+public class Persoon {
+
+    private String naam;
+    private int leeftijd;
+
+    public Persoon(String nm, int lft){
+        naam = nm;
+        leeftijd = lft;
+    }
+
+    public String toString(){
+        return naam + "; leeftijd " + leeftijd + " jaar";
+    }
+}
